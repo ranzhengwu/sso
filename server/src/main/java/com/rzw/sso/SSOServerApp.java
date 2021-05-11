415a238b106e5e5c99bab2c063076588cd9ef9c1
ranzhengwu <ranzhengwu@163.com>
2021-05-11 09:01:01 +0800

    //

--- server/src/main/java/com/rzw/sso/SSOServerApp.java
+++ server/src/main/java/com/rzw/sso/SSOServerApp.java
@@ -3,11 +3,13 @@ package com.rzw.sso;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 @SpringBootApplication
 public class SSOServerApp {
 
     public static void main(String[] args) {
         //
-        SpringApplication.run(SSOServerApp.class,args);
+        SpringApplication.run(SSOServerApp.class,args);;
     }
 }
